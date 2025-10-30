2dfa2e0d8c2e90e1c87176b8e304f3c14270b3c8
anotherhafuog <freesolokills@gmail.com>
2025-10-30 12:55:54 -0700

    Added tests for drawLine and drawLines, used drawPoints for vector monitor-style corners, and removed drawing unneccesary black background

--- SDLGraphicsTest/drawProgram.cpp
+++ SDLGraphicsTest/drawProgram.cpp
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <SDL_rect.h>
 #undef main
 #include <iostream>
 using namespace std;
@@ -13,15 +14,40 @@ int main() {
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_CreateWindowAndRenderer(windowSizeX*4, windowSizeY*4, 0, &window, &renderer);
 	SDL_RenderSetScale(renderer, 4, 4); //initialize window
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //set window to black
 
-	SDL_RenderClear(renderer); //clear color
-
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 0); //set color to blue
+	SDL_SetRenderDrawColor(renderer, 0, 0, 128, 255); //set color to blue
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_ADD); // sets colors to additive blend
 	SDL_RenderDrawPoint(renderer, windowSizeX / 2, windowSizeY / 2); //draw blue pixel in the middle of the screen
+	SDL_RenderDrawLine(renderer, 100, 100, 200, 200); //draw line
+
+	struct SDL_Point rectPoints[5];
+	rectPoints[0].x = 300;
+	rectPoints[0].y = 300;
+
+	rectPoints[1].x = 320;
+	rectPoints[1].y = 300;
+
+	rectPoints[2].x = 320;
+	rectPoints[2].y = 320;
+
+	rectPoints[3].x = 300;
+	rectPoints[3].y = 320; 
+
+	rectPoints[4].x = rectPoints[0].x;
+	rectPoints[4].y = rectPoints[0].y; // define points for square
+	//todo: figure out a way to avoid storing duplicate points
+
+	SDL_RenderDrawLines(renderer, rectPoints, 5); //draw square
+	SDL_RenderDrawPoints(renderer, rectPoints, 4); //draw points on edges for atari-style vector effects
+
+	/* todo:
+	* test using drawrect for line thickness and less aliasing
+	* try scaling draw rect
+	* attempt animation using SDL time
+	*/
 
 	SDL_RenderPresent(renderer);
-	SDL_Delay(10000); //keep open window for 10000 ticks
+	SDL_Delay(5000); //keep open window for 5000 ticks
 
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);

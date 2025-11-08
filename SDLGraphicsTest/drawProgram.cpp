defca7812c9da7a559863d06e18ddaae2b4e5c0b
anotherhafuog <freesolokills@gmail.com>
2025-11-08 12:21:18 -0800

    Created a function to draw nice thick lines

--- SDLGraphicsTest/drawProgram.cpp
+++ SDLGraphicsTest/drawProgram.cpp
@@ -2,11 +2,46 @@
 #include <SDL_rect.h>
 #undef main
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+void DrawVec(SDL_Renderer* renderer, SDL_Point pts[], float thickness, SDL_Color color) {
+
+	float x0 = pts[0].x;
+	float x1 = pts[1].x;
+
+	float y0 = pts[0].y;
+	float y1 = pts[1].y;// define points from struct
+
+	float dx = x1 - x0;
+	float dy = y1 - y0;
+	float length = sqrt((dx * dx) + (dy * dy));
+
+	float ptx = (-dy / length) * (thickness / 2.0f);
+	float pty = (dx / length) * (thickness / 2.0f);
+
+	struct SDL_Vertex verts[4];
+	verts[0].position = { x0 + ptx, y0 + pty };
+	verts[1].position = { x1 + ptx, y1 + pty };
+	verts[2].position = { x1 - ptx, y1 - pty };
+	verts[3].position = { x0 - ptx, y0 - pty }; // Compute points for rect
+
+	for (int i = 0; i < 4; ++i) {
+		verts[i].color = color;
+	} //set color
+
+	const int indices[6] = { 0, 1, 2, 2, 3, 0 };
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 128, 255); //set color to blue for now
+
+	SDL_RenderGeometry(renderer, nullptr, verts, 4, indices, 6); //draw
+}
 
 int main() {
 	int windowSizeX = 640;
 	int windowSizeY = 480;//defines window size
+
+	SDL_Color atariBlue = {0, 0, 128, 255}; //defines color
 
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
@@ -40,32 +75,22 @@ int main() {
 	SDL_RenderDrawLines(renderer, rectPoints, 5); //draw square
 	SDL_RenderDrawPoints(renderer, rectPoints, 4); //draw points on edges for atari-style vector effects
 
-	struct SDL_Point angPoints[8];
+	struct SDL_Point angPoints[2];
 	angPoints[0].x = 400;
 	angPoints[0].y = 200;
 
 	angPoints[1].x = 420;
 	angPoints[1].y = 205;
 
-	angPoints[2].x = (angPoints[1].x);
-	angPoints[2].y = (angPoints[1].y + 1);
-
-	angPoints[3].x = (angPoints[0].x);
-	angPoints[3].y = (angPoints[0].y + 1);
+	struct SDL_Point otherPoints[2] = {440, 200, 420, 205}; // initializer points
 	
-	// this is too many points to store!
+	DrawVec(renderer, angPoints, 4.0, atariBlue);
+	DrawVec(renderer, otherPoints, 4.0, atariBlue); // draw new thicker lines
 
-	//SDL_RenderDrawLines(renderer, angPoints, 4);//draw thick line
-
-	//^v these should draw the same result, but top results in overlapping pixels
-
-	SDL_RenderDrawLine(renderer, angPoints[0].x, angPoints[0].y++, angPoints[1].x, angPoints[1].y++); //draw line
-	SDL_RenderDrawLine(renderer, angPoints[0].x, angPoints[0].y--, angPoints[1].x, angPoints[1].y--); //draw line
-	SDL_RenderDrawLine(renderer, (angPoints[0].x)+40, angPoints[0].y++, angPoints[1].x-1, angPoints[1].y++); //draw line
-	SDL_RenderDrawLine(renderer, (angPoints[0].x)+40, angPoints[0].y, angPoints[1].x-1, angPoints[1].y); //draw line
 	/* todo:
 	* attempt scaling
 	* attempt animation using SDL time
+	* make line intersections look good
 	*/
 
 	SDL_RenderPresent(renderer);
